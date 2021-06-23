bd454d89b0e21d049d513ca5ae91bf38faf4d09f
Ruslan Chuprov <chrv1828@gmail.com>
2021-06-23 16:35:14 +0300

    Asserts

--- src/main/java/org/example/Credentials.java
+++ src/main/java/org/example/Credentials.java
@@ -4,6 +4,6 @@ public class Credentials {
     public static final String EMAIL = "chrv1828+1@gmail.com";
     public static final String FIRSTNAME = "Ruslann";
     public static final String LASTNAME = "Chuprov";
-    public static final String PASSWORD = "QWaszxc123";
+    public static final String PASSWORD = "QWaszxc12345";
 
 }
